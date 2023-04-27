eed3b762cdb81bbb5abf8c0c11531b375ab9d2ff
Dmytro Furtas <df@codegeeks.solutions>
2023-04-27 18:54:57 +0300

    Init the project

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { useContainer } from 'class-validator';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  const port = process.env.PORT || 3000;
+  const document = SwaggerModule.createDocument(
+    app,
+    new DocumentBuilder().setTitle('Tagging API').setVersion('1.0').build(),
+  );
+  SwaggerModule.setup('swagger', app, document);
+  await app.listen(port, () => {
+    console.log(`=== Running on port ${port} ===`);
+  });
+}
+bootstrap();
